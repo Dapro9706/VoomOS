4dab720a1bd33b186ce15bf77fd215366dffa81d
Dapro9706 <swaminathshhiju@gmail.com>
2021-12-18 15:33:20 +0530

    bugs eh?

--- drivers/port.h
+++ drivers/port.h
@@ -1,8 +1,3 @@
-unsigned char port_byte_in (unsigned short port);
-void port_byte_out (unsigned short port, unsigned char data);
-unsigned short port_word_in (unsigned short port);
-void port_word_out (unsigned short port, unsigned short data);
-
 unsigned char port_byte_in (unsigned short port) {
     unsigned char result;
     __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
